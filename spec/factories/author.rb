720138d993adf8d1340b74cdfb9332963f98ce7c
PhilipeeX <dev.philipe@outlook.com>
2023-07-19 10:34:26 -0300

    test(factorybot): factory for books and authors

--- spec/factories/author.rb
+++ spec/factories/author.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :author do
-    nome { "John Doe" }
-    biography { "A talented writer with many published works." }
-    nationality { "USA" }
-    birth_date { Date.new(1980, 1, 1) }
-    literary_genre { "Fiction" }
-    bibliography { "List of published books" }
-    contacts { "johndoe@example.com" }
+    nome { "Isaac Asimov" }
+    biography { "Isaac Asimov was a prolific science fiction writer known for his groundbreaking works in the genre. With a keen interest in science and a talent for storytelling, he captivated readers with his imaginative stories and thought-provoking ideas. Asimov's extensive bibliography includes popular works like the Foundation series, the Robot series, and many more." }
+    nationality { "American" }
+    birth_date { Date.new(1920, 1, 2) }
+    literary_genre { "Science Fiction" }
+    bibliography { "Foundation series, Robot series, I, Robot, The Caves of Steel, The Naked Sun, etc." }
+    contacts { "isaacasimov@example.com" }
   end
 end
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :book do
-    author { nil }
-    title { "MyString" }
-    isbn { "MyString" }
-    description { "MyText" }
-    publish_year { 1 }
-    price { "9.99" }
+    association :author, factory: :author, nome: "Isaac Asimov"
+    title { "Foundation" }
+    isbn { "9781234567890" }
+    description { "An institute founded by psychohistorian Hari Seldon to preserve the best of galactic civilization after the collapse of the Galactic Empire." }
+    publish_year { 1951 }
+    price { "39.99" }
   end
 end
