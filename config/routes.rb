799b73d665317f1df3aa7a3ac78e74e2ff5ea4b2
PhilipeeX <dev.philipe@outlook.com>
2023-07-20 10:39:47 -0300

    feat(routes): configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :assemblies
+  resources :parts
+  resources :supplier_accounts
+  resources :suppliers
   resources :books
   resources :authors
+
+  resources :suppliers do
+    resources :parts, only: [:new, :create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
