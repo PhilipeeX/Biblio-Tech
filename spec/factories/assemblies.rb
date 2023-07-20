e51a816ae4a57b8097c203b790733d2ecdfb4307
PhilipeeX <dev.philipe@outlook.com>
2023-07-20 10:39:23 -0300

    test(initial tests): created

---
+++ spec/factories/assemblies.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :assembly do
+    title { "MyString" }
+    description { "MyText" }
+    date { "2023-07-20" }
+    version { "MyString" }
+  end
+end
---
+++ spec/factories/supplier_accounts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :supplier_account do
+    account { 1 }
+    supplier { nil }
+  end
+end
