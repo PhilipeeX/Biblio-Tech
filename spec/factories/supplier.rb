73cf34f2f20d0b93ce23975804d81474ab4b377c
PhilipeeX <dev.philipe@outlook.com>
2023-09-05 21:55:53 -0300

    test(supplier): rspec tests created

--- spec/factories/supplier.rb
+++ spec/factories/supplier.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :supplier do
     name { Faker::Bank.name }
-    cnpj { '35.035.289/0001-69' }
+    cnpj { Faker::Company.brazilian_company_number(formatted: true) }
   end
 end
